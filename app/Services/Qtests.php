2fc8d0e254daa0d55760277e0b8191ea8ff7ef03
Ugur Batuhan Penekli <me@ubpenekli.com>
2022-11-30 04:11:44 +0300

    service refactored

--- app/Services/Qtests.php
+++ app/Services/Qtests.php
@@ -8,68 +8,120 @@ use Illuminate\Support\Facades\Storage;
 class Qtests
 {
     private $http;
+    private $token_key;
 
     public function __construct()
     {
         $this->http = Http::baseUrl(config('qtests.base'));
     }
 
-    public function getToken()
+    public function getToken($email, $password)
     {
-        $user = $this->http->post('/authentication', [
-            'email' => config('qtests.email'),
-            'password' => config('qtests.pass')
-        ])->object();
+        $response = $this->http->post('/authentication', [
+            'email' => $email,
+            'password' => $password
+        ]);
 
-        return $user->token_key;
+        if ($response->ok()) {
+            if ($response->status() === 200) {
+                return collect([
+                    'success' => true,
+                    'token' => $response->object(),
+                    'message' => 'You are logged in successfully.'
+                ]);
+            } else {
+                return collect([
+                    'success' => false,
+                    'token' => null,
+                    'message' => 'Your credentials are not valid.'
+                ]);
+            }
+        } else {
+            return collect([
+                'success' => false,
+                'token' => null,
+                'message' => 'Login action failed on API server.'
+            ]);
+        }
+    }
+
+    public function setToken($token_key)
+    {
+        $this->token_key = $token_key;
     }
 
     public function authors()
     {
-        return $this->http->get('/authors')->object();
+        $response = $this->http->withToken($this->token_key)->get('/authors');
+        if ($response->ok() && $response->status() === 200) {
+            return collect([
+                'success' => true,
+                'message' => null,
+                'data' => $response->object()
+            ]);
+        } else {
+            return collect([
+                'success' => false,
+                'message' => 'Token is invalid.',
+                'data' => null,
+            ]);
+        }
     }
 
     public function author($author_id)
     {
-        return $author = $this->http->get('/authors/' . $author_id)->object();
+        $response = $this->http->withToken($this->token_key)->get('/author/' . $author_id);
+        if ($response->ok() && $response->status() === 200) {
+            return collect([
+                'success' => true,
+                'message' => null,
+                'data' => $response->object()
+            ]);
+        } else {
+            return collect([
+                'success' => false,
+                'message' => 'Token is invalid.',
+                'data' => null,
+            ]);
+        }
     }
 
     public function removeAuthor($author_id)
     {
         $author = $this->author($author_id);
-        $books = collect($author->books);
+        $books = collect($author['data']->books);
         if ($books->isNotEmpty()) {
             if ($this->http->delete('/authors/' . $author_id)->ok()) {
-                return [
+                return collect([
                     'success' => true,
                     'message' => null
-                ];
+                ]);
             } else {
-                return [
+                return collect([
                     'success' => false,
                     'message' => 'Deleting action failed on API server.'
-                ];
+                ]);
             }
         } else {
-            return [
+            return collect([
                 'success' => false,
                 'message' => 'Author cannot be deleted because the author has books.'
-            ];
+            ]);
         }
     }
 
     public function removeBook($book_id)
     {
         if ($this->http->delete('/books/' . $book_id)->ok()) {
-            return [
+            return collect([
                 'success' => true,
                 'message' => null
-            ];
+            ]);
         } else {
-            return [
+            return collect([
                 'success' => false,
                 'message' => 'Deleting action failed on API server.'
-            ];
+            ]);
         }
     }
 }
--- config/qtests.php
+++ config/qtests.php
@@ -1,6 +1,4 @@
 <?php
 return [
     'base' => env('QTESTS_API_URL', 'https://symfony-skeleton.q-tests.com/api/v2'),
-    'email' => env('QTESTS_API_EMAIL', 'ahsoka.tano@q.agency'),
-    'pass' => env('QTESTS_API_PASS', 'Kryze4President'),
 ];
